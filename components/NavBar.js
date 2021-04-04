d4c0bd04c6dca172da02f10bf78df40b10ac2caa
Megan <calvem2@cs.washington.edu>
2021-04-03 23:21:25 -0700

    remove unnecessary import

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import styles from '../styles/Navbar.module.css'
 import { useRouter } from 'next/router'
-const uuid = require('uuid');
 
 export default function NavBar() {
     const router = useRouter();
