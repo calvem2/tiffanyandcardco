beabfa081860521cb42d889a3582eaad892fb924
Megan <calvem2@cs.washington.edu>
2021-04-04 20:57:00 -0700

    reformat about page

--- pages/about.js
+++ pages/about.js
@@ -11,18 +11,28 @@ export default function About(props) {
           <link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/274/love-letter_1f48c.png"/>
         </Head>
         <NavBar></NavBar>
+        <div id={style.start}>
+                <img
+                    src="/images/gallery.jpg" 
+                />
+                <div id={style["start-info"]}>
+                    <p>
+                        hi, i'm tiffany
+                    </p>
+                    <a href="#form-start"><span></span></a>
+                </div>
+            </div>
         <div className={style.flex}>
-          <div id={style.image}>
+          {/* <div id={style.image}>
               <img src="/images/profile.jpeg"/>
-          </div>
+          </div> */}
           <div id={style.info}>
-              <h1 id={style.heading}>hi, i'm tiffany</h1>
               <p>Thanks for stopping by my card portfolio!</p>
               <p>I was introduced to the world of card making when I started working at a stamp store in college, and I've been stampin' ever since!</p>
               <p>I love making cards for birthdays, holidays, and everything in between for my friends and family. Check out my gallery to see what I've made so far!</p>
           </div>
         </div>
-        <Footer></Footer>
+        {/* <Footer></Footer> */}
       </div>
     )
 }
